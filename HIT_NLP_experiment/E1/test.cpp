ceb09966e256750129f06ca0869a6f1a1c2ccae3
cx0627 <2919730935@qq.com>
2020-11-23 00:57:39 +0800

    sugar

--- HIT_NLP_experiment/E1/test.cpp
+++ HIT_NLP_experiment/E1/test.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
-#include <stdio.h>
-#include <string.h>
-#include <locale>
-#include <wchar.h>
-using namespace std;
-
-wchar_t str[5005];
-
-int main(){
+#include <clocale>//c->ctype,cmath;locale->local environment
+using std::cout;
+using std::endl;
+using std::wcin;
+using std::wcout;
+wchar_t s[50045];
+int main()
+{   
     freopen("test.in","r",stdin);
-    // setlocale(LC_ALL,"zh_CN.GBK");
-    printf("yes");
-    wscanf(L"%ls",str);
-    int len = wcslen(str);
-    printf("yes");
-    cout << len << endl;
-    // for(int i=0;i<len;i++)
-        // wcout << sizeof(str[0]) << endl;
-    printf("%d\n",str[0]);
-    printf("%lc\n",str[1]*256+str[0]);
-    return 0;
+    freopen("test.out","w",stdout);
+	setlocale(LC_ALL, "chs");//输出wchar_t类型的必要条件
+	wcin >> s;
+    wcout << s[0] << endl;
+ 
+return 0;
 }
