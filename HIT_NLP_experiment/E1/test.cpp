37489bc4480015609b42f2ab8983e16306f8be79
cx0627 <2919730935@qq.com>
2020-11-29 20:52:13 +0800

    sugar

--- HIT_NLP_experiment/E1/test.cpp
+++ HIT_NLP_experiment/E1/test.cpp
@@ -8,7 +8,7 @@ wchar_t s[50045];
 int main()
 {   
     freopen("test.in", "r", stdin);
-    freopen("test.out", "w", stdout);
+    freopen("test1.out", "w", stdout);
 	setlocale(LC_ALL, "chs");//输出wchar_t类型的必要条件
 	wcin >> s;
     cout << "yes" << endl;
